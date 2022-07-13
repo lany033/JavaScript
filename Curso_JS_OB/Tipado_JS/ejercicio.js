23db82c001794ed730fbd7e3d557504e8f1a67e8
Melanie <mmantilla930@gmail.com>
2022-07-13 17:40:43 -0500

    Avance modulo 2

---
+++ Curso_JS_OB/Tipado_JS/ejercicio.js
@@ -0,0 +1,17 @@
+const nombre = "Melanie";
+
+const edad = 29;
+
+const desarrollador = true;
+
+const nacimiento = new Date(1993, 2, 20)
+
+const libro = {
+    titulo: "Cancion de hielo y fuego",
+    autor: "George R. R. Martin",
+    anyo: 1996,
+    tomos: 5,
+    paginas: 564,
+    personajePrincipal: "John Snow",
+
+}
