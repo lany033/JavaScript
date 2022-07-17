934c1b4ba9cbd0c37be9abe6c4cd1caf342a830c
Melanie <mmantilla930@gmail.com>
2022-07-17 17:35:03 -0500

    conjuntos

---
+++ Curso_JS_OB/Sets y Objetos/conjuntos.js
@@ -0,0 +1,11 @@
+const myFamiliy = [ "Anna" , "Diego","Juan","Kira","Kimmi"]
+
+const miSet = new Set(myFamiliy)
+
+miSet.add("Melanie")
+
+console.log(miSet);
+
+miSet.add("Javascript")
+
+console.log(miSet);
