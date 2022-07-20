196e5404c857694c1a08e7e59c61380ade2cda58
Melanie <lany033@hotmail.com>
2022-07-20 18:17:02 -0500

    test

--- Curso_JS_OB/Modulos/index.js
+++ Curso_JS_OB/Modulos/index.js
@@ -2,4 +2,4 @@ import { multiplicacion } from './funcion/controller.js';
 import chalk from 'chalk'; 
 
 const result = multiplicacion(1,2,4,5)
-console.log(chalk.green("resultado: " + result));
+console.log(chalk.green("resultado: " + result))
